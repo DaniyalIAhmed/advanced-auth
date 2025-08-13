9e41551d705c45c396bbbab4003a2a259bf42426
DaniyalIAhmed <m.daniyal0201@gmail.com>
2025-08-13 20:24:20 +0500

    Merging 0.0.1

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id: string;
+  userName: string;
+  email: string;
+  isVerified: boolean;
+  createdAt: string;  // Or Date, depending on usage
+  updatedAt: string;
+  lastLogin: string; 
+  __v: number;
+}
